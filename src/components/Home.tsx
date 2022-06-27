dcadb4d4c04c1be2d72a459ce00780adc5223c49
isaoxenaar <isamoxenaar@gmail.com>
2022-06-27 14:37:46 +0200

    FrontEndForOscar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,7 +9,8 @@ const Home = () => {
 
   return (
     <section className="Home--main">
-
+        <text className="Home--text">welcome to this todo app, get lost in all the tasks, omg, no its supposed to help you get some order in your life!</text>
+        <img className="Home--img" src="https://essential.blue/wp-content/uploads/2021/07/IMG_6432-scaled-e1632669305502.jpg"alt=""></img>
     </section>
   );
 }
--- src/components/SubGallery.tsx
+++ src/components/SubGallery.tsx
@@ -1,28 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import "../CSS/SubGallery.css";
 import { SubType } from "../Types/SubType";
+import { ToDoType } from '../Types/ToDoType';
 
-const SubGallery = () => {
-    const [subs, setSubs] = useState<SubType[]>();
+interface IProps {
+    todoItem: ToDoType
+}
+
+const SubGallery: FC<IProps>= ({todoItem}) => {
+    const [loading, setLoading ] = useState<boolean>(true);
+    const [subs, setSubs] = useState<SubType[]>([{id: 0, title: "", text: "", deadline: "", cost: 10, todoid: 0 }]);
 
     const fetchSubs = async () => {
-        const request = await fetch("https://localhost:7039/api/ToDo")
-        //const request = await fetch("https://todoby.azurewebsites.net/api/Sub")
+        //const request = await fetch("https://localhost:7039/api/Sub")
+        const request = await fetch("https://todoby.azurewebsites.net/api/Sub")
         const response = await request.json();
         console.log(response);
         setSubs(response);
+        setLoading(false);
     }
 
     useEffect(() => {
         fetchSubs();
-    }, [])
+    }, [loading])
 
-        if(!subs)
+        if(loading)
             return <div>loading in todogallery</div>
         return (
             <div>
                 subs: {subs.map(sub => <p>{sub.title}</p>)}
-            
             </div>
         )
 }
